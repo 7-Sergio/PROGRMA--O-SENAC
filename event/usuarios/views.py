074ae507acea8eb99d1ba973f1a9ad5bdd7ab391
Sergio <sergio.htp7@gmail.com>
2023-09-20 19:43:45 -0300

    q

--- event/usuarios/views.py
+++ event/usuarios/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
